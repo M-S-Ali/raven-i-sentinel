d5df59d5755a5d30b2f6d6454f5e94c871588d17
M S Ali <84390575+M-S-Ali@users.noreply.github.com>
2024-12-05 20:17:56 +0400

    gui strucutre almost visualised

--- software/bms_gui_bq76925_atmega328p_6s2_20a/src/gui.py
+++ software/bms_gui_bq76925_atmega328p_6s2_20a/src/gui.py
@@ -9,7 +9,7 @@ cellBalance=[]
 #-----------------------------------------------------------------------------------------------------Cell Voltage Frame
 class cellVoltageFrame(customtkinter.CTkFrame):
     def __init__(self,master,number_of_cells,height):
-        super().__init__(master)
+        super().__init__(master,fg_color="transparent")
         for i in range(number_of_cells):
             self.grid_columnconfigure(i, weight=1)
             self.cellvoltage = customtkinter.CTkProgressBar(
@@ -31,7 +31,7 @@ class cellVoltageFrame(customtkinter.CTkFrame):
 #-----------------------------------------------------------------------------------------------------Cell Balancing Frame
 class cellBalancingFrame(customtkinter.CTkFrame):
     def __init__(self,master,number_of_cells):
-        super().__init__(master)
+        super().__init__(master,fg_color="transparent")
         for i in range(number_of_cells):
             self.grid_columnconfigure(i,weight=1)
             self.cellBalance = customtkinter.CTkButton(self,width=20,height=20,text="")
@@ -39,18 +39,47 @@ class cellBalancingFrame(customtkinter.CTkFrame):
             cellBalance.append(self.cellBalance)
 #-----------------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------------Cell Balancing Frame
-class cellLabel(customtkinter.CTkLabel):
+#-----------------------------------------------------------------------------------------------------Cell Label Frame
+class cellLabel(customtkinter.CTkFrame):
     def __init__(self,master,number_of_cell):
-        super().__init__(master,text="")
+        super().__init__(master,fg_color="transparent")
         for i in range(number_of_cell):
             self.grid_columnconfigure(i,weight=1)
             self.label = customtkinter.CTkLabel(self,text="CB"+str(i))
             self.label.grid(row=0,column=i,padx=0,pady=0)
 #-----------------------------------------------------------------------------------------------------------------------
 
+#-----------------------------------------------------------------------------------------------------Current Label Frame
+class currentLabel(customtkinter.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master,fg_color="transparent")
+        for i in range(6):
+            self.grid_columnconfigure(i,weight=1)
+        self.label = customtkinter.CTkLabel(self,text = "16A", height=80,width=80,font=("Arial",50))
+        self.label.grid(row=0,column=0)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------Current Progress Frame
+class currentProgress(customtkinter.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master,fg_color="transparent")
+        self.currentProgress = customtkinter.CTkProgressBar(
+                self,
+                width=300,
+                height=30,
+                corner_radius=0
+            )
+        self.currentProgress.grid(
+                row=0,
+                column=0,
+                padx=0,
+                pady=0
+            )
+        
 
 
+#-----------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -64,15 +93,27 @@ class app(customtkinter.CTk):
         for i in range(6):
             self.grid_columnconfigure(i, weight=1)
 
+        self.currentProgress = currentProgress(self)
+        self.currentProgress.grid(row=1,column=0,columnspan=6)
+        
 
-        self.cellBalancingFrame = cellBalancingFrame(self,7)
-        self.cellBalancingFrame.grid(row=0,column=0,rowspan=1,columnspan=7,sticky='ew')
+        self.currentLabel = currentLabel(self)
+        self.currentLabel.grid(row=0,column=0,columnspan=6)
 
-        self.cellLabel = cellLabel(self,number_of_cell=7)
-        self.cellLabel.grid(row=1,column=0,rowspan=1,columnspan=7,sticky="ew")
+        self.voltagelbel = currentLabel(self)
+        self.voltagelbel.grid(row=2,column=0,columnspan=3)
+        
+        self.templabel = currentLabel(self)
+        self.templabel.grid(row=2,column=3,columnspan=3)
 
-        self.cellvoltageframe = cellVoltageFrame(self,number_of_cells=7,height=300)
-        self.cellvoltageframe.grid(row=2,column=0,rowspan=1,columnspan=7,sticky="ew")
+        self.cellBalancingFrame = cellBalancingFrame(self,6)
+        self.cellBalancingFrame.grid(row=3,column=0,rowspan=1,columnspan=6,sticky='ew')
+
+        self.cellLabel = cellLabel(self,number_of_cell=6)
+        self.cellLabel.grid(row=4,column=0,rowspan=1,columnspan=6,sticky="ew")
+
+        self.cellvoltageframe = cellVoltageFrame(self,number_of_cells=6,height=300)
+        self.cellvoltageframe.grid(row=5,column=0,rowspan=1,columnspan=6,sticky="ew")
 
 root = app()
 cellVoltage[4].set(0.9)
